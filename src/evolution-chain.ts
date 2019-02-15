75193a1ed7825486ce740575e6c85eba08dc7033
Christian Bonilla <christianb354@gmail.com>
2019-02-14 22:45:40 -0500

    Removed unnecessary files

--- src/evolution-chain.ts
+++
@@ -1,6 +0,0 @@
-import { Chain } from './chain';
-
-export interface EvolutionChain {
-	chain: Chain;
-	id: number;
-}
--- src/pokemon-info.ts
+++
@@ -1,7 +0,0 @@
-export interface PokemonInfo {
-	evolution_chain: {
-		url: string
-	};
-	id: number;
-	name: string;
-}
