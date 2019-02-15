f7469a579e6e8ec137fd33647368a7f18d08c543
Christian Bonilla <christianb354@gmail.com>
2019-02-14 22:47:31 -0500

    Prepare for chapter

--- src/index.ts
+++
@@ -1,8 +0,0 @@
-import * as dotEnv from 'dotenv';
-// Do this before evaluating the server class file
-dotEnv.config();
-
-import { Server } from './server';
-
-const server = new Server();
-server.start();
